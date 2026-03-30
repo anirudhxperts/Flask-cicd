2a0060623fe897dc0d6a68f66f26ff4bb5a094fe
Yash-at-Unify <yash@unifyxperts.com>
2026-03-30 15:30:42 +0530

    Fix: Change welcome message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from my CI/CD pipeline! 🚀"
+    return "Welcome to anirudhra's tutorial 🚀"
 
 @app.route("/health")
 def health():
